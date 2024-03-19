3f175c6982810be96a529069be0605f1a5927e50
ᨏ <gagan.mishra@gmail.com>
2024-03-19 00:20:00 -0700

    random string generation

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
 use clap::Parser;
+use rand::Rng;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    name: String,
+    #[arg(short, long, default_value_t = 16)]
+    length: i32,
+}
 
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+static CHARACTERS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789$&!@#";
+
+fn generate_password(length: i32) -> String {
+    let mut rng = rand::thread_rng();
+    let charset: Vec<char> = CHARACTERS.chars().collect();
+    let password: String = (0..length)
+        .map(|_| rng.gen_range(0..charset.len()))
+        .map(|idx| charset[idx])
+        .collect();
+    password
 }
 
 fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+    println!("Generated password: {}", generate_password(args.length));
 }
